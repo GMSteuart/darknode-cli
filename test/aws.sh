6235bca5b140683a75923db43d9c67c6bd932f90
tok-kkk <yunshi@republicprotocol.com>
2020-06-12 13:57:11 +1000

    tsting

--- test/aws.sh
+++ test/aws.sh
@@ -1,20 +1,33 @@
 #  Deployment tests
+name="testing-aws-$1"
 
 # Deploy a darknode using AWS
-darknode up --aws --name testing-aws --aws-access-key  $aws_access_key --aws-secret-key $aws_secret_key --tags mainnet,testing
-darknode list
-darknode down testing-aws -f
+darknode up --aws --name $name --aws-access-key  $aws_access_key --aws-secret-key $aws_secret_key --tags mainnet,testing
+if [ "$?" -ne "0" ]; then
+    echo "failed to deploy darknode"
+    exit 1
+fi
+if ! darknode list; then
+    echo "failed to list node"
+    exit 1
+fi
+if ! darknode down $name -f; then
+    echo "failed to destroy node"
+    exit 1
+fi
 
 # Return error when not providing a provider name
 darknode up
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test without providing a provider name"
+    echo "failed to pass test without providing a provider name"
+    exit 1
 fi
 
 # Return error when not providing a node name
 darknode up --aws
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test without providing a node name"
+    echo "failed to pass test without providing a node name"
+    exit 1
 fi
 
 # TODO : deploy node with specific region and instance type
@@ -22,33 +35,40 @@ fi
 # Return error when providing an empty name
 darknode up --aws --name ""
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing an empty name"
+    echo "failed to pass test when providing an empty name"
+    exit 1
 fi
 
 # Return error when providing an invalid network name
-darknode up --aws --name testing-aws --network invalid-network
+darknode up --aws --name $name --network invalid-network
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing an invalid network"
+    echo "failed to pass test when providing an invalid network"
+    exit 1
 fi
 
 # Return error when providing an invalid AWS credentials
-darknode up --aws --name testing-aws --aws-access-key key --aws-secret-key value
+darknode up --aws --name $name --aws-access-key key --aws-secret-key value
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing invalid credentials"
+    echo "failed to pass test when providing invalid credentials"
+    exit 1
 fi
 rm -rf $HOME/.darknode/darknodes/testing-aws
 
 # Return error when providing an invalid aws region
-darknode up --aws --name testing-aws --aws-access-key  $aws_access_key --aws-secret-key $aws_secret_key --aws-region invalid-region
+darknode up --aws --name $name --aws-access-key  $aws_access_key --aws-secret-key $aws_secret_key --aws-region invalid-region
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing invalid region"
+    echo "failed to pass test when providing invalid region"
+    exit 1
 fi
 
 # Return error when providing an invalid aws instance type
-darknode up --aws --name testing-aws --aws-access-key  $aws_access_key --aws-secret-key $aws_secret_key --aws-instance invalid-instance
+darknode up --aws --name $name --aws-access-key  $aws_access_key --aws-secret-key $aws_secret_key --aws-instance invalid-instance
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing invalid instance"
+    echo "failed to pass test when providing invalid instance"
+    exit 1
 fi
-darknode down testing-aws -f
-
+if ! darknode down $name -f; then
+    echo "failed to destroy node"
+    exit 1
+fi
 echo "All AWS tests passed!"
--- test/do.sh
+++ test/do.sh
@@ -1,14 +1,26 @@
 #  Deployment tests
+name="testing-do-$1"
 
 # Deploy a darknode using DO
-darknode up -do -name testing-do --do-token $do_token --tags testing
-darknode list
-darknode down testing-do -f
+darknode up -do -name $name --do-token $do_token --tags testing
+if [ "$?" -ne "0" ]; then
+    echo "failed to deploy darknode"
+    exit 1
+fi
+if ! darknode list; then
+    echo "failed to list node"
+    exit 1
+fi
+if ! darknode down $name -f; then
+    echo "failed to destroy node"
+    exit 1
+fi
 
 # Return error when providing no name
 darknode up --do
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test without providing a node name"
+    echo "failed to pass test without providing a node name"
+    exit 1
 fi
 
 # TODO : deploy node with specific region and instance type
@@ -16,33 +28,41 @@ fi
 # Return error when providing an empty name
 darknode up --do --name ""
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing an empty name"
+    echo "failed to pass test when providing an empty name"
+    exit 1
 fi
 
 # Return error when providing an invalid network name
-darknode up --do --name testing-do --do-token $do_token --network invalid-network
+darknode up --do --name $name --do-token $do_token --network invalid-network
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing an invalid network"
+    echo "failed to pass test when providing an invalid network"
+    exit 1
 fi
 
 # Return error when providing an invalid digital ocean token
-darknode up --do --name testing-do --do-token invalid-token
+darknode up --do --name $name --do-token invalid-token
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing invalid credentials"
+    echo "failed to pass test when providing invalid credentials"
+    exit 1
 fi
 rm -rf $HOME/.darknode/darknodes/testing-do
 
 # Return error when providing an invalid digital ocean region
-darknode up --do --name testing-do --do-token $do_token --do-region invalid-region
+darknode up --do --name $name --do-token $do_token --do-region invalid-region
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing invalid region"
+    echo "failed to pass test when providing invalid region"
+    exit 1
 fi
 
 # Return error when providing an invalid digital ocean droplet type
-darknode up --do --name testing-do --do-token $do_token --do-droplet invalid-droplet-type
+darknode up --do --name $name --do-token $do_token --do-droplet invalid-droplet-type
 if [ "$?" -eq "0" ]; then
-    exit "failed to pass test when providing invalid instance"
+    echo "failed to pass test when providing invalid instance"
+    exit 1
 fi
-darknode down testing-do -f
+if ! darknode down $name -f; then
+    echo "failed to destroy node"
+    exit 1
+fi
 
 echo "All DO tests passed!"
